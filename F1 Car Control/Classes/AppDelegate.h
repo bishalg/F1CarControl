ac7880e5e703a751591469b4efb49fe909c38675
Bishal Ghimire <bishal.ghimire@gmail.com>
2014-05-10 08:22:02 +0545

    Initial Commit

---
+++ F1 Car Control/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  F1 Car Control
+//
+//  Created by Bishal Ghimire on 5/10/14.
+//  Copyright Big B Soft 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ F1 Car Control/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  F1 Car Control
+//
+//  Created by Bishal Ghimire on 5/10/14.
+//  Copyright Big B Soft 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
